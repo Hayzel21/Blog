aa783f429c3ed3c477d424c5a4109721c51781a2
Hayzel21 <hlwin827@gmail.com>
2023-07-14 20:06:53 +0630

    BLog Finish

--- backend/user_edit.php
+++ backend/user_edit.php
@@ -21,18 +21,22 @@
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-        $profile_arr = $_FILES['profile'];
+        $profile_arr = $_FILES['new_profile'];
+        $old_profile = $_POST['profile'];
 
         // echo "$name and $email and $password";
         // print_r ($profile_arr);
 
         if (isset($profile_arr) && $profile_arr['size'] >0) {
            $dir = 'images/';
-           $photo = $dir.$profile['name'];
-           $tmp_name = $profile ['tmp_name'];
-           move_uploaded_file($tmp_name, $photo);
+           $profile = $dir.$profile_arr['name'];
+           $tmp_name = $profile_arr ['tmp_name'];
+           move_uploaded_file($tmp_name, $profile);
 
-        };
+        }else{
+
+            $profile = $old_profile;
+        }
 
         // $sql = "INSERT INTO users (name,email,password,profile) VALUES (:name,:email,:password,:profile)";
 
@@ -43,7 +47,7 @@
         $stmt->bindParam (':name',$name);
         $stmt->bindParam (':email',$email);
         $stmt->bindParam (':password',$password);
-        $stmt->bindParam (':profile',$photo);
+        $stmt->bindParam (':profile',$profile);
         $stmt->execute();
 
         
@@ -95,21 +99,23 @@
 
                    <nav class="my-3">
                         <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                            <button class="nav-link active" id="nav-photo-tab" data-bs-toggle="tab" data-bs-target="#nav-photo" type="button" role="tab" aria-controls="nav-photo" aria-selected="true">Photo</button>
+                            <button class="nav-link active" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="true">Photo</button>
 
-                            <button class="nav-link" id="nav-new_photo-tab" data-bs-toggle="tab" data-bs-target="#nav-new_photo" type="button" role="tab" aria-controls="nav-new_photo" aria-selected="false">New Photo</button>
+                            <button class="nav-link" id="nav-new_profile-tab" data-bs-toggle="tab" data-bs-target="#nav-new_profile" type="button" role="tab" aria-controls="nav-new_profile" aria-selected="false">New Photo</button>
 
                             
                         </div>
                     </nav>
                     <div class="tab-content" id="nav-tabContent">
 
-                        <div class="tab-pane fade show active" id="nav-photo" role="tabpanel" aria-labelledby="nav-photo-tab" tabindex="0">
-                            <img src="<?= $users['profile'] ?>" alt="">
+                        <div class="tab-pane fade show active" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+                            <img src="<?= $user['profile'] ?>" alt="" class="img-fluid w-50 h-50 py-5">
+
+                            <input type="hidden" name="profile" value="<?= $user['profile'] ?>">
                         </div>
 
-                        <div class="tab-pane fade" id="nav-new_photo" role="tabpanel" aria-labelledby="nav-new_photo-tab" tabindex="0">
-                            <input type="file" name="profile" id="profile" class="form-control mt-3" value="<?=  $user['profile']?>">
+                        <div class="tab-pane fade" id="nav-new_profile" role="tabpanel" aria-labelledby="nav-new_profile-tab" tabindex="0">
+                            <input type="file" name="new_profile" id="new_profile" class="form-control mt-3">
                         </div>
 
 
